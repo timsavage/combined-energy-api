35982f6d883c86bc08a62b3650ed3e9796c482b0
Tim Savage <tim@savage.company>
2022-11-02 01:41:45 +1100

    Add ReadingsIterator that handles API behaviours to iterate over readings

---
+++ src/combined_energy/helpers.py
@@ -0,0 +1,72 @@
+import logging
+from collections import deque
+from datetime import datetime, timedelta
+from typing import Optional, AsyncIterator
+
+from .client import CombinedEnergy
+from .models import Readings
+
+LOGGER = logging.getLogger(__package__)
+
+now = datetime.now
+
+
+class ReadingsIterator:
+    """
+    Iterator that returns readings with a certain interval specified
+    """
+
+    __slots__ = ("client", "increment", "initial_delta", "_last_end", "_empty")
+
+    def __init__(
+        self,
+        client: CombinedEnergy,
+        *,
+        increment: int,
+        initial_delta: timedelta = None,
+        log_session_reset_count: int = 3
+    ):
+        self.client = client
+        self.increment = increment
+        self.initial_delta = initial_delta
+
+        self._last_end: Optional[datetime] = None
+        # Prefill with False
+        self._empty = deque([False] * log_session_reset_count, log_session_reset_count)
+
+    @property
+    def next_range_start(self) -> Optional[datetime]:
+        """
+        Calculate the next range start value
+        """
+        if self._last_end:
+            return self._last_end
+        if self.initial_delta:
+            return now() - self.initial_delta
+
+    async def _check_for_log_session_restart(self):
+        """
+        Check if a start log session message is required.
+
+        This is a strange behaviour of this API that requires the client to
+        call start new log session. In the dashboard app this is covered up
+        with a dialog that pops up periodically.
+        """
+        # Reset if deque is full of True statuses
+        if all(self._empty):
+            LOGGER.info("Restart log session...")
+            await self.client.start_log_session()
+
+    async def __aiter__(self) -> AsyncIterator[Readings]:
+        while True:
+            await self._check_for_log_session_restart()
+
+            readings = await self.client.readings(
+                self.next_range_start, None, self.increment
+            )
+
+            # Update state
+            self._empty.append(readings.range_count == 0)
+            self._last_end = readings.range_end
+
+            yield readings
---
+++ tests/test_helpers.py
@@ -0,0 +1,105 @@
+from datetime import datetime, timedelta
+from unittest.mock import Mock, AsyncMock
+
+import pytest
+
+from combined_energy import helpers
+
+
+class TestReadingsIterator:
+    def test_next_range_start__where_no_request_made(self):
+        target = helpers.ReadingsIterator(
+            AsyncMock(helpers.CombinedEnergy), increment=10
+        )
+
+        actual = target.next_range_start
+
+        assert actual is None
+
+    def test_next_range_start__where_successful_request_made(self):
+        target = helpers.ReadingsIterator(
+            AsyncMock(helpers.CombinedEnergy), increment=10
+        )
+        target._last_end = datetime(2022, 2, 22, 22, 22, 22)
+
+        actual = target.next_range_start
+
+        assert actual == datetime(2022, 2, 22, 22, 22, 22)
+
+    def test_next_range_start__where_timedelta_set(self, monkeypatch):
+        monkeypatch.setattr(helpers, "now", lambda: datetime(2022, 2, 22, 22, 22, 22))
+
+        target = helpers.ReadingsIterator(
+            AsyncMock(helpers.CombinedEnergy),
+            increment=10,
+            initial_delta=timedelta(minutes=22),
+        )
+
+        actual = target.next_range_start
+
+        assert actual == datetime(2022, 2, 22, 22, 00, 22)
+
+    @pytest.mark.asyncio
+    async def test_check_for_log_session_restart__check_behaviour(self):
+        mock_client = AsyncMock(helpers.CombinedEnergy)
+        target = helpers.ReadingsIterator(
+            mock_client, increment=10, log_session_reset_count=2
+        )
+
+        await target._check_for_log_session_restart()
+        mock_client.start_log_session.assert_not_called()
+
+        target._empty.append(False)
+        target._empty.append(True)
+
+        await target._check_for_log_session_restart()
+        mock_client.start_log_session.assert_not_called()
+
+        target._empty.append(True)
+
+        await target._check_for_log_session_restart()
+        mock_client.start_log_session.assert_called()
+
+    @pytest.mark.asyncio
+    async def test_iterator(self):
+        mock_client = AsyncMock(
+            helpers.CombinedEnergy,
+            readings=AsyncMock(
+                side_effect=[
+                    Mock(range_count=1, range_end=datetime(2022, 2, 22, 22, 00, 21)),
+                    Mock(range_count=1, range_end=datetime(2022, 2, 22, 22, 00, 22)),
+                    Mock(range_count=0, range_end=datetime(2022, 2, 22, 22, 00, 22)),
+                    Mock(range_count=0, range_end=datetime(2022, 2, 22, 22, 00, 22)),
+                    Mock(range_count=3, range_end=datetime(2022, 2, 22, 22, 00, 25)),
+                ]
+            ),
+        )
+        target = helpers.ReadingsIterator(
+            mock_client, increment=10, log_session_reset_count=2
+        )
+        iterator = aiter(target)
+
+        # Check first request
+        await anext(iterator)
+        assert target.next_range_start == datetime(2022, 2, 22, 22, 00, 21)
+        mock_client.start_log_session.assert_not_called()
+
+        # Check second request
+        await anext(iterator)
+        assert target.next_range_start == datetime(2022, 2, 22, 22, 00, 22)
+        mock_client.start_log_session.assert_not_called()
+
+        # Check third request
+        await anext(iterator)
+        assert target.next_range_start == datetime(2022, 2, 22, 22, 00, 22)
+        mock_client.start_log_session.assert_not_called()
+
+        # Check forth request
+        await anext(iterator)
+        assert target.next_range_start == datetime(2022, 2, 22, 22, 00, 22)
+        mock_client.start_log_session.assert_not_called()
+
+        # Check fifth request
+        await anext(iterator)
+        assert target.next_range_start == datetime(2022, 2, 22, 22, 00, 25)
+        mock_client.start_log_session.assert_called()
